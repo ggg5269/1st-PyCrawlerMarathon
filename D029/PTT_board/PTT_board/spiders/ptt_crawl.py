183c0382edff51906babdfd3acc06976a8bc1379
ggg5269 <37011770+ggg5269@users.noreply.github.com>
2020-01-17 12:30:32 +0800

    037

--- D029/PTT_board/PTT_board/spiders/ptt_crawl.py
+++ D029/PTT_board/PTT_board/spiders/ptt_crawl.py
@@ -6,12 +6,16 @@ from pathlib import Path
 from pprint import pprint
 from ..items import PTTArticleItem
 
+
 class PttcrawlerSpider(scrapy.Spider):
-    name = 'PTTCrawler'
-    allowed_domains = ['www.ptt.cc']
-    start_urls = ['https://www.ptt.cc/bbs/Gossiping/M.1557928779.A.0C1.html']
-    cookies = {'over18': '1'}
-    
+    name = "PTTCrawler"
+    allowed_domains = ["www.ptt.cc"]
+    start_urls = ["https://www.ptt.cc/bbs/Gossiping/M.1557928779.A.0C1.html"]
+    cookies = {"over18": "1"}
+
     def start_requests(self):
         for url in self.start_urls:
             yield scrapy.Request(url=url, callback=self.parse, cookies=self.cookies)
+
+    def parse(self, response):
+        pass
